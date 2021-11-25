08c57551e1bc905d68db4937b1316acc165b8d82
unknown <lh77x7@gmail.com>
2021-11-25 14:01:25 +0100

    Update 1-16.c
    exercise 1,2,3 and 5

--- ch6/exc/1-16.c
+++ ch6/exc/1-16.c
@@ -3,10 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int zad1();
-int zad2();
-int zad3();
-int zad4();
+char zad1(char *tablicaLiter);
+void zad2();
+void zad3();
+void gotox(int x);
+void zad4();
 int zad5();
 int zad6();
 int zad7();
@@ -19,6 +20,51 @@ int zad13();
 int zad14();
 int zad15();
 int zad16();
+
+// dane do zadan
+// 6.1
+char tablicaLiter[]="abcdefghijklmnopqrstuvwxyz";
+
+// 6.2 - 6.3
+char ch;
+
+// 6.4
+
+
+// 6.5
+
+
+// 6.6
+
+
+// 6.7 
+
+
+// 6.8
+
+
+// 6.9
+
+
+// 6.10
+
+
+// 6.11
+
+
+// 6.12
+
+
+// 6.13
+
+
+// 6.14
+
+
+// 6.15
+
+
+// 6.16
 
 
 int main(void){
@@ -36,7 +82,7 @@ int main(void){
         {
         
         case 1:
-            zad1();
+            zad1(tablicaLiter);
             break;
         case 2:
             zad2();
@@ -96,24 +142,88 @@ int main(void){
     return 0;
 }
 
-int zad1(){
-    printf("1!!!\n");
+void gotox(int x){
+    for(x; x > 0; --x)
+        putchar(' ');
 }
 
-int zad2(){
-    printf("2!!!\n");
+char zad1(char tablicaLiter[]){
+    // wpisanie danych
+    /*printf("Wpisz 26 liter: ");
+    for (int i = 0; i < 26; i++){
+        scanf("%c", &tablicaLiter[i]);
+    }*/
+    // wypisanie danych
+    for(int i = 0; i < 26; i++){
+        printf("%c", tablicaLiter[i]);
+    }
+    printf("\n");
+    return *tablicaLiter;
+    
 }
 
-int zad3(){
-    printf("3!!!\n");
+void zad2(){
+    char ch = '$';
+    for(int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < i + 1; j++) {
+            printf("%c", ch);
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
 
-int zad4(){
-    printf("4!!!\n");
+void zad3(){
+    char ch = 'A';
+    for (int i = 0; i < 5; i++)
+    {
+        for (ch = 'A'; ch < ('A' + i + 1); ch++){
+            printf("%c", ch);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void zad4(){
+
+    /* choinka - uproszczona wersja, 
+    
+        **
+       ****
+      ******
+     ********
+    **********
+
+    dla liczba = 5;
+
+     */
+
+    /*
+    int liczba;
+    printf("Podaj liczbe wierszy: ");
+    scanf("%d", &liczba);
+    for(int i = 1; i < liczba; i++){
+        gotox(liczba - i);
+        for (int j = 0; j < i; j++) printf("**");
+        putchar('\n');
+    }
+    */    
 }
 
 int zad5(){
-    printf("5!!!\n");
+    int minimum, maksimum;
+    printf("Podaj minimum: "); scanf("%d", &minimum);
+    printf("Podaj maksimum: "); scanf("%d", &maksimum);
+    if(minimum < maksimum) {
+        printf("n n*n n*n*n\n");
+        for(int index = minimum; index <= maksimum; index++) {
+            printf("%d %d %d\n", index, index*index, index*index*index);
+        }
+    } else {
+        printf ("Min musi byc wieksze od maks.\n");
+    }
 }
 
 int zad6(){
