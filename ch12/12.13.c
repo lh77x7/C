acbc9a7b39a5ac68bbd5fc56ebe4cf8f9e2e5bfe
unknown <lh77x7@gmail.com>
2021-12-23 22:19:49 +0100

    examples 12.11 - 12.14 chap 12
    examples 12.11 - 12.14 chapter 12 - done

---
+++ ch12/12.13.c
@@ -0,0 +1,27 @@
+// wielorzut.c - wielokrotny rzut koscmi
+// kompilowac wraz z rzutkosc.c - 12.11.c
+#include <stdio.h>
+#include <stdlib.h> // funkcja srand()
+#include <time.h>   // funkcja time()
+#include "../include/rzutkosc.h" // potrzeba funkcji rzucaj_n_razy() 
+//i zmiennej liczby rzutow
+
+int main(void){
+    int rzuty, wynik;
+    int scianki;
+    srand((unsigned int) time(0));  // losowe ziarno
+    printf("Podaj liczbe scianek, 0 oznacza koniec.\n");
+    while (scanf("%d", &scianki) == 1 && scianki > 0)
+    {
+        printf("Ile rzutow?\n");
+        scanf("%d", &rzuty);
+        wynik = rzucaj_n_razy(rzuty, scianki);
+        printf("Wyrzucono razem %d uzywajac %d %d - sciennych kosci.\n",
+        wynik, rzuty, scianki);
+        printf("Podaj liczbe scianej, 0 oznacza koniec.\n");
+    }
+    printf("Funkcja rzucaj() zostala wywolana %d razy.\n", liczba_rzutow);  //zmienna zew.
+    printf("ZYCZE DUZO SZCZESCIA!\n");
+    
+    return 0;
+}
