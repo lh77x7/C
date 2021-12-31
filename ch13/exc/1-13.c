35ced59059b6a922c0a6f0abb1b759cfffbc5d94
unknown <lh77x7@gmail.com>
2021-12-31 10:51:05 +0100

    exc 6 chap 13
    exc 6 chapter 13 - done

--- ch13/exc/1-13.c
+++ ch13/exc/1-13.c
@@ -1,6 +1,8 @@
 // 1-13.c - rozwiazania do cwiczen 1-13 rozdzial 13
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h> //strcpy(), strcat()
 
 void zad1();
 void zad2();
@@ -80,7 +82,7 @@ int main(void){
 3 -     NOT DONE!
 4 -     NOT DONE!
 5 -     NOT DONE!
-6 -     NOT DONE!
+6 -     DONE
 7 -     NOT DONE!
 8 -     NOT DONE!
 9 -     NOT DONE!
@@ -115,7 +117,31 @@ void zad5(){
 }
 
 void zad6(){
-
+    FILE *we, *wy;  // deklarowanie 2 wskazników plikowych
+    int ch;
+    char nazwaWe[40]; // miejsce dla nazwy pliku wejsciowego
+    char nazwa[40]; // miejsce dla nazwy pliku wyjsciowego
+    int licznik = 0;
+    getchar();
+    printf("Podaj nazwe pliku: ");
+    gets(nazwaWe);
+    if((we = fopen(nazwaWe, "r")) == NULL)
+    {
+        fprintf(stderr, "Nie moglem otworzyc pliku \"%s\".\n", nazwaWe);
+        exit(2);
+    }
+    strcpy(nazwa, nazwaWe); // kopiowanie nazwy pliku do tablicy
+    strcat(nazwa, ".new");  // dopisanie .new do nazwy
+    if((wy = fopen(nazwa, "w")) == NULL)
+    {   // otwiera plik do zapisu
+        fprintf(stderr, "Nie mozna otworzyc pliku wyjsciowego.\n");
+        exit(3);
+    }
+    while((ch = getc(we)) != EOF)
+        if(licznik++ %3 == 0)
+            putc(ch, wy);   // wyswietla co 3 znak
+    if(fclose(we) != 0 || fclose(wy) != 0)
+        fprintf(stderr, "Blad przy zamykaniu plikow.\n");
 }
 
 void zad7(){
