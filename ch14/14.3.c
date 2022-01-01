f531e52b8ba02a157d0c960e2722d62d9c3d3457
unknown <lh77x7@gmail.com>
2022-01-01 15:02:39 +0100

    exa 14.3, 14.4 chap 14
    exa 14.3, 14.4 chapter 14 - done

---
+++ ch14/14.3.c
@@ -0,0 +1,47 @@
+// znajom.c - przykład wykorzystania struktury zagnieżdżonej
+#include <stdio.h>
+#define DL 20
+const char * teksty[5] =
+{
+    "   Dziekuje za cudowny wieczor,  ",
+    "Nie przypuszczalam, ze ",
+    "moze byc tak wspanialym facetem. Musimy umowic sie",
+    "na pyszny ",
+    " i spedzic ze soba kilka milych chwil"
+};
+
+struct daneos{ // pierwszy szablon
+    char imie[DL];
+    char nazw[DL];
+};
+
+struct facet{ // drugi szablon
+    struct daneos person;   // struktura zagnieżdżona
+    char ulub_jedz[DL];
+    char zawod[DL];
+    float dochody;
+};
+
+int main(void){
+    struct facet gosc = {  // inicjalizacja zmiennej
+        {"chip", "hyperlink"},
+        "talerz chipsow",
+        "trener osobisty",
+        36827.00
+};
+printf("%s - moj drogi, \n\n", gosc.person.imie);
+printf("%s%s.\n", teksty[0], gosc.person.imie);
+printf("%s%s\n", teksty[1], gosc.zawod);
+printf("%s\n", teksty[2]);
+printf("%s%s%s", teksty[3], gosc.ulub_jedz, teksty[4]);
+if(gosc.dochody > 150000.0)
+    puts("!!");
+else if(gosc.dochody > 75000.0)
+    puts("!");
+else
+    puts(".");
+printf("\n%40s%s\n", " ", "Do zobaczenia wkrotce,");
+printf("%40s%s\n", " ", "Szalala");
+
+return 0;
+}
---
+++ ch14/14.4.c
@@ -0,0 +1,41 @@
+// znajom.c - wykorzystuje wskaznik do struktury
+#include <stdio.h>
+#define DL 20
+struct daneos{
+    char imie[DL];
+    char nazw[DL];
+};
+
+struct facet{
+    struct daneos person;
+    char ulub_jedz[DL];
+    char zawod[DL];
+    float dochody;
+};
+
+int main(void){
+    struct facet gosc[2] = {
+        {
+        {"chip", "hyperlink"},
+        "talerz chipsow",
+        "trener osobisty",
+        36827.00
+        },
+        {
+        {"norbert", "brzuchacz"},
+        "mus lososiowy",
+        "redaktor brukowca",
+        148500.00
+        }
+    };
+    struct facet *on;   // oto wskaznik do struktury
+    printf("adres #1: %p #2: %p\n", &gosc[0], &gosc[1]);
+    on = &gosc[0];  // ustawia wskaznik
+    printf("wskaznik #1: %p #2: %p\n", on, on + 1);
+    printf("on->dochody ma wartosc %.2f $: (*on).dochody ma wartosc %.2f $\n", on->dochody, (*on).dochody);
+    on++;   // wskazuje na nastepna strukture
+    printf("on->ulub_jedz ma wartosc %s: on->person.nazw ma wartosc %s\n",
+    on->ulub_jedz, on->person.nazw);
+
+    return 0;
+}
