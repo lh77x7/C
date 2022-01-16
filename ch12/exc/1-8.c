c804e6147172902369245f192416d35ebdcdbacb
unknown <lh77x7@gmail.com>
2022-01-16 10:13:01 +0100

    exc chapter 12 - tasks content
    exc chapter 12 - content for all tasks

--- ch12/exc/1-8.c
+++ ch12/exc/1-8.c
@@ -1,5 +1,133 @@
 // 1-8.c - rozwiazania do cwiczen 1-8 rozdzial 12
 
+/*
+
+1. Przepisz program z listingu 12.4 tak, aby nie korzystał ze zmiennych globalnych.
+
+2. Zużycie paliwa jest zwykle podawane w USA w milach na galon a w Europie w litrach
+na 100 km. Oto część programu, który pyta użytkownika o wybór trybu (metryczny 
+badź US), a następnie gromadzi dane i wylicza zużycie paliwa:
+
+// pe12-2b.c
+
+#include <stdio.h>
+#include "pe12-2a.h"
+int main(void){
+    int tryb;
+
+    printf("Wybierz: 0 - system metryczny, 1 - system US: ");
+    scanf("%d", &tryb);
+    while(tryb >= 0)
+    {
+        wybierz_tryb(tryb);
+        pobierz_dane();
+        wyswietl_dane();
+        printf("Wybierz: 0 - system metryczny, 1 - system US");
+        printf("(-1 aby zakonczyc):");
+        scanf("%d", &tryb);
+    }
+    printf("Koniec.\n");
+    return 0;
+}
+
+Oto przykładowe wyjściowe programu:
+
+    Wybierz: 0 - system metryczny, 1 - system US: 0
+    Wprowadz przebyta odleglosc w kilometrach: 600
+    Wprowadz zuzyte paliwo w litach: 78.8
+    Zuzycie paliwa wynioslo 13.13 litrow na 100 km.
+    Wybierz: 0 - system metryczny, 1 - system US (-1 aby zakonczyc): 1
+    Wprowadz przebyta odleglosc w milach: 434
+    Wprowadz zuzyte paliwo w galonach: 12.7
+    Zuzycie paliwa wynioslo 34.2 mil na galon.
+    Wybierz: 0 - system metryczny, 1 - system US (-1 aby zakonczyc): 3
+    Podano nieprawidlowa wartosc. Wybrano system 1(US).
+    Wprowadz przebyta odleglosc w milach: 388
+    Wprowadz zuzyte paliwo w galonach: 15.3
+    Zuzycie paliwa wynioslo 25.4 mil na galon.
+    Wybierz: 0 - system metryczny, 1 - system US (-1 aby zakonczyc): -1
+    Koniec.
+
+Jeśli użytkownik wybierze niewłaściwy tryb, program powinien to skomentować
+i użyć ostatnio wybranego trybu. Program powinien działać po dołączeniu
+do niego pliku nagłówkowego pe12-2a.h i pliku pe12-2a.c. Plik z kodem źródłowym
+powinien definiować trzy zmienne o zasięgu plikowym i łączności wewnętrznej.
+Niech jedna reprezentuje tryb, druga dystans, a trzecia zużyte paliwo. 
+Funkcja pobierz_dane() ma prosić o dane zgodnie z wybranym trybem i przechowywać
+wprowadzone przez użytkownika dane w zmiennych o zasięgu plikowym.
+Funkcja wyswietl_dane() niech oblicza o wyświetla zużycie paliwa w oparciu 
+o wybrany tryb.
+
+3. Zmodyfikuj ponownie program opisany w ćwiczeniu 2 tak, by używał wyłącznie
+zmiennych automatycznych. Niech program posiada taki sam interfejs użytkownika,
+to znaczy powinien prosić użytkownika, by wpisał tryb itd. Będziesz musiał
+skorzystać jednak z innych wywołań funkcji.
+
+4. Napisz i sprawdź w pętli funkcję, która zwraca ile razy została wywołana.
+
+5. Napisz program, który generuje listę 100 losowych liczb z przedziału
+od 1 do 10 w porządku malejącym. (Możesz w tym celu wykorzystać algorytm przedstawiony
+w rodziale 11 dla typu int. W tym przypadku po prostu posortuj same liczby).
+
+6. Napisz program, który generuje 1000 losowych liczb z przedziału od 1 do 10.
+Nie zachowuj ani nie pokazuj liczb, tylko wyświetl liczbę razy dana liczba została
+wybrana. Niech program korzysta z 10 różnych ziaren. Czy liczby te pojawią się
+w różnej ilości? Możesz użyć funkcji z tego rodziału albo standardowych funkcji
+ANSI C: rand() i srand(), które działają tak jak nasze funkcje. Jest to jeden
+ze sposobów, by sprawdzić losowość określonego generatora liczb losowych.
+
+7. Napisz program, który zachowuje się jak modyfikacja listingu 12.13, którą omówiliśmy
+po pokazaniu jego wyników. Niech program zwraca następujące wartości:
+
+    Wprowadź liczbe kolejek: wybierz q aby zakonczyc.
+    18
+    Ile scian i ile kosci?
+    6 3
+    Oto 18 kolejek rzytow 3 6-sciennymi kostkami:
+        12 10 6 9 8 14 8 15 9 14 12 17 11 7 10
+    Wprowadz liczbe kolejek; wybierz q aby zakonczyc.
+    q
+
+8. Oto fragment programu:
+
+// pe12-8.c
+
+#include <stdio.h>
+int * stworz_tablice(int elem, int wart);
+void pokaz_tablice(const int tab[], int n);
+int main(void){
+    int *wt;
+    int rozmiar;
+    int wartosc;
+    printf("Podaj liczbe elementow: ");
+    scanf("%d", &rozmiar);
+    while(rozmiar > 0)
+    {
+        printf("Podaj wartosc poczatkowa: ");
+        scanf("%d", &wartosc);
+        wt = stworz_tablice(rozmiar, wartosc);
+        if(wt)
+        {
+            pokaz_tablice(wt, rozmiar);
+            free(wt);
+        }
+        printf("Podaj liczbe elementow (<1 - koniec): ");
+        scanf("%d", &rozmiar);
+    }
+    printf("Koniec.\n");
+    return 0;
+}
+
+Uzupełnij program przez udostępnienie definicji funkcji stworz_tablice()
+i pokaz_tablice(). Funkcja stworz_tablice() wymaga dwoch argumentow.
+Pierwszy z nich jest liczba elementow tablicy liczb calkowitych (int), 
+a drugi oznacza wartość, która jest przypisana każdemu z elementów. 
+Funkcja używa malloc(), by stworzyć tabelę o odpowiednim rozmiarze, przypisać
+każdemu elementowi wskazana wartość i zwrócić wskaźnik do tablicy. Funkcja
+pokaz_tablice() wyswietla zawartosc - po osiem liczb w wierszu.
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
