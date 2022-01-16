f6c35982304080902def7f6c38a059fab21a8ac9
unknown <lh77x7@gmail.com>
2022-01-16 14:36:05 +0100

    exc 12,13 chap 13
    exc 12,13 chapter 13 - done

--- ch13/exc/1-13.c
+++ ch13/exc/1-13.c
@@ -4,7 +4,12 @@
 #include <stdlib.h>
 #include <string.h> //strcpy(), strcat()
 #include <ctype.h>  // toupper()
+#include <math.h>
 #define MAX 40
+#define ROWS 20 
+#define COLS 30
+#define LEVELS 10
+const char trans[LEVELS + 1] = " .':~*=&%@";
 
 void zad1();
 void zad2();
@@ -19,6 +24,9 @@ void zad10();
 void zad11();
 void zad12();
 void zad13();
+void MakePic(int data[][COLS], char pic[][COLS], int rows);
+void init(char arr[][COLS], char ch);
+void deglitch(int data[][COLS]);
 
 int main(void){
     int choice;
@@ -90,8 +98,8 @@ int main(void){
 9 -     DONE
 10 -    DONE
 11 -    DONE
-12 -    NOT DONE!
-13 -    NOT DONE!
+12 -    DONE
+13 -    DONE
 
 */
 
@@ -254,9 +262,137 @@ void zad11(){
 }
 
 void zad12(){
+    int row, col;
+    int picIn[ROWS][COLS];
+    char picOut[ROWS][COLS];
+    char fileName[81];
+    FILE * infile;
 
+    init(picOut, 'S');
+
+    printf("Wpisz nazwe pliku: ");
+    scanf("%80s", fileName);
+    if ((infile = fopen(fileName, "r")) == NULL)
+    {
+        fprintf(stderr, "Nie moge otworzyc pliku.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (row = 0; row < ROWS; row++)
+        for (col = 0; col < COLS; col++)
+            fscanf(infile, "%d", &picIn[row][col]);
+    if (ferror(infile))
+    {
+        fprintf(stderr, "Blad przesylu danych z pliku.\n");
+        exit(EXIT_FAILURE);
+    }
+    MakePic(picIn, picOut, ROWS);
+
+    for (row = 0; row < ROWS; row++)
+    {
+        for (col = 0; col < COLS; col++)
+            putchar(picOut[row][col]);
+        putchar('\n');
+    }
 }
 
 void zad13(){
-    
+    int row, col;
+    int picIn[ROWS][COLS];
+    char picOut[ROWS][COLS];
+    char fileName[81];
+    FILE * infile;
+
+    init(picOut, 'S');
+
+    printf("Wpisz nazwe pliku: ");
+    scanf("%80s", fileName);
+    if ((infile = fopen(fileName, "r")) == NULL)
+    {
+        fprintf(stderr, "Nie moglem otworzyc pliku.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (row = 0; row < ROWS; row++)
+        for (col = 0; col < COLS; col++)
+            fscanf(infile, "%d", &picIn[row][col]);
+    if (ferror(infile))
+    {
+        fprintf(stderr, "Blad przysylu danych z pliku.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    deglitch(picIn);
+
+    MakePic(picIn, picOut, ROWS);
+
+    for (row = 0; row < ROWS; row++)
+    {
+        for (col = 0; col < COLS; col++)
+            putchar(picOut[row][col]);
+        putchar('\n');
+    }
+}
+
+void init(char arr[][COLS], char ch)
+{
+    int r, c;
+    for (r = 0; r < ROWS; r++)
+        for (c = 0; c < COLS; c++)
+            arr[r][c] = ch;
+}
+
+void MakePic(int data[][COLS], char pic[][COLS], int rows)
+{
+    int row, col;
+    for (row = 0; row < rows; row++)
+        for (col = 0; col < COLS; col++)
+            pic[row][col] = trans[data[row][col]];
+}
+
+void deglitch(int data[][COLS])
+{
+	double total;
+	int count;
+
+	for (int i = 0; i < ROWS; i++)
+		for (int j = 0; j < COLS; j++)
+		{
+			total = 0;
+			count = 0;
+			if (i + 1 < ROWS)
+			{
+				if (abs(data[i][j] - data[i + 1][j]) <= 1)
+					continue;
+
+				total += data[i + 1][j];
+				count++;
+			}
+			if (j + 1 < COLS)
+			{
+				if (abs(data[i][j] - data[i][j + 1]) <= 1)
+					continue;
+
+				total += data[i][j + 1];
+				count++;
+			}
+			if (i > 0)
+			{
+				if (abs(data[i][j] - data[i - 1][j]) <= 1)
+					continue;
+
+				total += data[i - 1][j];
+				count++;
+			}
+			if (j > 0)
+			{
+				if (abs(data[i][j] - data[i][j - 1]) <= 1)
+					continue;
+
+				total += data[i][j - 1];
+				count++;
+			}
+
+			data[i][j] = (int) rint(total / count);
+		}
 }
