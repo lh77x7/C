daa0215770d66789b46a3e8ca699472733fe8716
unknown <lh77x7@gmail.com>
2022-01-20 01:19:05 +0100

    exc 4,5 chap 15
    exc 4,5 chapter 15 - done

--- ch15/exc/1-7.c
+++ ch15/exc/1-7.c
@@ -92,7 +92,10 @@ void zad6();
 void zad7();
 int binToDec(char *);
 int iloscBitow(int n);
-
+int zawartoscBitu(int, int);
+char *doBinar (int n, char *wsk);
+void pokazBinar(const char * lan);
+void obrocLewo(unsigned int n, int ilosc);
 
 int main(void){
     int choice;
@@ -138,8 +141,8 @@ int main(void){
 1 -     DONE
 2 -     DONE
 3 -     DONE
-4 -     NOT DONE!
-5 -     NOT DONE!
+4 -     DONE
+5 -     DONE
 6 -     NOT DONE!
 7 -     NOT DONE!
 
@@ -157,7 +160,9 @@ void zad2(){
 }
 
 void zad3(){
+    
     int n;
+    
     printf("Wprowadz liczbe: ");
     scanf("%d", &n);
     printf("%d\n", n);
@@ -166,10 +171,27 @@ void zad3(){
 }
 
 void zad4(){
+    
+    int n, numer;
+    
+    printf("Wprowadz liczbe: ");
+    scanf("%d", &n);
+    printf("Wprowadz numer bitu, ktorego numer chcesz sprawdzic: ");
+    scanf("%d", &numer);
 
+    printf("Zawartosc bitu o numerze %d w liczbe %d wynosi: %d\n", numer, n, zawartoscBitu(n, numer));
 }
 
 void zad5(){
+    
+    unsigned int n;
+    int ilosc;
+
+    printf("Wprowadz liczbe, ktora chcesz obrocic: ");
+    scanf("%d", &n);
+    printf("O ile miejsc chcesz przesunac w lewo?");
+    scanf("%d", &ilosc);
+    obrocLewo(n, ilosc);
 
 }
 
@@ -182,17 +204,22 @@ void zad7(){
 }
 
 int binToDec(char *wbin){
+    
     int liczba = 0;
+    
     while(*wbin != '\0')
     {
         liczba = 2 * liczba + (*wbin++ - '0');
     }
+
     return liczba;
 }
 
 int iloscBitow(int n){
+    
     int liczbaBitow = 0;
     int i = 0;
+    
     while(i < 32)
     {
         if((01&n) == 1)
@@ -202,5 +229,69 @@ int iloscBitow(int n){
         n >>= 1;
         i++;        
     }
+
     return liczbaBitow;
+}
+
+int zawartoscBitu(int n, int numer) {
+
+    int zawartoscBitu = 0;
+    int i = 0;
+    
+    while(i < 32)
+    {
+        if((01 & n) &&  i == numer)
+        {
+            zawartoscBitu = 1 << numer;
+        }
+        n >>= 1;
+        i++;
+    }
+
+    return zawartoscBitu;
+}
+
+char * doBinar(int n, char *wsk)
+{
+    int i;
+    static int liczba = 8*sizeof(int);
+    for (i=liczba-1; i>=0; i--, n>>=1) {
+        wsk[i] = (01 & n) + '0';
+    }
+    wsk[liczba] = '\0';
+    return wsk;
+}
+
+// wyswietl lancuch binarny w blokach po 4
+void pokazBinar(const char * lan)
+{
+    int i = 0;
+    while (lan[i]) {
+        putchar(lan[i]);
+        if (++i % 4 == 0 && lan[i]) {
+            putchar(' ');
+        }
+    }
+    putchar('\n');
+}
+
+void obrocLewo(unsigned int n, int ilosc){
+
+    char bin[32];
+    unsigned int temp;
+
+    // pierwsze wyswietlenie
+    doBinar(n, bin);
+    pokazBinar(bin);
+
+    // tutaj zaczyna sie cala operacja obrocenia w lewo
+    temp = n >> (32 - ilosc);
+    n <<= ilosc;
+
+    n = n|temp;
+
+    // drugie wyswietlenie - po operacji docelowej funkcji
+    doBinar(n, bin);
+    pokazBinar(bin);
+
 }
--- ch15/exc/15.2.c
+++ ch15/exc/15.2.c
@@ -2,9 +2,9 @@
 
 #include <stdio.h>
 
-int bin_to_dec(char *wbin);
-char *do_binar (int n, char *wsk);
-void pokaz_binar(const char * lan);
+int binToDec(char *wbin);
+char *doBinar (int n, char *wsk);
+void pokazBinar(const char * lan);
 
 int main(void) {
     
@@ -24,36 +24,36 @@ int main(void) {
     printf("Wprowadz drugi lancuch binarny:\n");
     scanf("%s", second);
     
-    first_dec = bin_to_dec(first);
-    second_dec = bin_to_dec(second);
+    first_dec = binToDec(first);
+    second_dec = binToDec(second);
     
     printf("Pierwsza liczba to: %d\n", first_dec);
-    do_binar(~first_dec, first_neg);
+    doBinar(~first_dec, first_neg);
     printf("~%d = ", first_dec);
-    pokaz_binar(first_neg);
-    printf("Druga liczba to: %d\n", bin_to_dec(second));
-    do_binar(~second_dec, second_neg);
+    pokazBinar(first_neg);
+    printf("Druga liczba to: %d\n", binToDec(second));
+    doBinar(~second_dec, second_neg);
     printf("~%d = ", second_dec);
-    pokaz_binar(second_neg);
+    pokazBinar(second_neg);
     putchar('\n');
     
     and = first_dec & second_dec;
     
     printf("%d & %d = ", first_dec, second_dec);
-    pokaz_binar(do_binar(and, dzialanie));
+    pokazBinar(doBinar(and, dzialanie));
     
     or = first_dec | second_dec;
     
     printf("%d | %d = ", first_dec, second_dec);
-    pokaz_binar(do_binar(or, dzialanie));
+    pokazBinar(doBinar(or, dzialanie));
     
     xor = first_dec ^ second_dec;
     
     printf("%d ^ %d = ", first_dec, second_dec);
-    pokaz_binar(do_binar(xor, dzialanie));
+    pokazBinar(doBinar(xor, dzialanie));
 }
 
-int bin_to_dec (char * wbin) {
+int binToDec (char * wbin) {
     
     int liczba = 0;
     while (*wbin != '\0')
@@ -64,7 +64,7 @@ int bin_to_dec (char * wbin) {
     return liczba;
 }
 
-char * do_binar(int n, char *wsk)
+char * doBinar(int n, char *wsk)
 {
     int i;
     static int liczba = 8*sizeof(int);
@@ -76,7 +76,7 @@ char * do_binar(int n, char *wsk)
 }
 
 // wyswietl lancuch binarny w blokach po 4
-void pokaz_binar(const char * lan)
+void pokazBinar(const char * lan)
 {
     int i = 0;
     while (lan[i]) {
