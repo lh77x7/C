b43682f99e9d975953fc23f875552bc48a28052c
unknown <lh77x7@gmail.com>
2022-01-23 11:48:19 +0100

    exa 4,5,6,7,8 chap 16
    examples 4, 5, 6, 7 and 8 chapter 16 - done

---
+++ ch16/16.7.c
@@ -0,0 +1,34 @@
+// daneos.c -- definiowanie funkcji
+#include <stdio.h>
+#include "daneos.h"  // dolacz plik naglowkowy
+
+// definiowanie funkcji
+void wprowadz(daneos *wskn){
+    
+    int i;
+
+    printf("Prosze podac imie: ");
+    fgets(wskn->imie, DL_LAN, stdin);
+    i = 0;
+    while (wskn->imie[i] != '\n' && wskn->imie[i] != '\0')
+        i++;
+    if(wskn->imie[i] == '\n')
+        wskn->imie[i] = '\0';
+    else
+        while (getchar() != '\n')
+            continue;
+    printf("Prosze podac nazwisko: ");
+    fgets(wskn->nazw, DL_LAN, stdin);
+    i = 0;
+    while (wskn->nazw[i] != '\n' && wskn->nazw[i] != '\0')
+        i++;
+    if(wskn->nazw[i] == '\n')
+        wskn->nazw[i] == '\0';
+    else
+        while(getchar() != '\n')
+            continue;
+}
+
+void wyswietl(const daneos *wskn){
+    printf("%s %s", wskn->imie, wskn->nazw);
+}
